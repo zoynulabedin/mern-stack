bb22531c9693bbdc8f6317b2c5a0b5adac8f5fad
Md zoynul abedin <33788041+zoynulabedin@users.noreply.github.com>
2022-06-29 12:51:49 +0600

    update

--- Dom-project/Github-multiuser/assets/js/app.js
+++ Dom-project/Github-multiuser/assets/js/app.js
@@ -2,7 +2,7 @@ const searchForm = document.getElementById('searchForm');
 const loadingBtn = document.getElementById('loadingBtn');
 const searchInput = document.getElementById('searchInput');
 const btnSearch = document.getElementById('btnSearch');
-const my_token = "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";
+const my_token = "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";
 searchForm.onsubmit = function(e) {
   e.preventDefault();
  const users =  fetch ('https://api.github.com/search/users?q=' + searchInput.value, {
