69a15585a9c6f79458b807a5d7b08ae77787439b
md zoynul abedin <33788041+zoynulabedin@users.noreply.github.com>
2022-12-06 18:47:16 +0600

    update comments

--- Dom-project/Github-multiuser/assets/js/app.js
+++ Dom-project/Github-multiuser/assets/js/app.js
@@ -1,22 +1,25 @@
-const searchForm = document.getElementById('searchForm');
-const loadingBtn = document.getElementById('loadingBtn');
-const searchInput = document.getElementById('searchInput');
-const btnSearch = document.getElementById('btnSearch');
-const my_token = "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";
-searchForm.onsubmit = function(e) {
-  e.preventDefault();
- const users =  fetch ('https://api.github.com/search/users?q=' + searchInput.value, {
-    'headers': {
-      'Authorization': `token ${my_token}` 
-    }
-  })
-  .then(response => response.json());
-  users.then(data => {
-    console.log(data);
-    const usersList = document.getElementById('usersList');
-    usersList.innerHTML = '';
-    data.items.forEach(user => {
-      const  list = `
+const searchForm = document.getElementById("searchForm");
+const loadingBtn = document.getElementById("loadingBtn");
+const searchInput = document.getElementById("searchInput");
+const btnSearch = document.getElementById("btnSearch");
+const my_token =
+	"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";
+searchForm.onsubmit = function (e) {
+	e.preventDefault();
+	const users = fetch(
+		"https://api.github.com/search/users?q=" + searchInput.value,
+		{
+			headers: {
+				Authorization: `token ${my_token}`,
+			},
+		}
+	).then((response) => response.json());
+	users.then((data) => {
+		console.log(data);
+		const usersList = document.getElementById("usersList");
+		usersList.innerHTML = "";
+		data.items.forEach((user) => {
+			const list = `
       <div class="col-lg-3 my-3 text-center">
       <div class="card p-3 shadow">
         <div class="profile-img">
@@ -33,12 +36,7 @@ searchForm.onsubmit = function(e) {
       </div>
     </div>
       `;
-      usersList.innerHTML += list;
-    });
-    
-  });
-
-
- 
-}
-
+			usersList.innerHTML += list;
+		});
+	});
+};
